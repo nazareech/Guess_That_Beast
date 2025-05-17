3cdab432147cf9ff1a2f3092857d1f0a5d1ce0a1
nazareech <nzrprotasov@gmail.com>
2025-05-17 14:44:59 +0300

    Create the initial main menu

--- src/main/java/com/example/guess_that_beast/HelloController.java
+++ src/main/java/com/example/guess_that_beast/HelloController.java
@@ -1,14 +1,34 @@
 package com.example.guess_that_beast;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.Button;
+import javafx.scene.text.Text;
 
 public class HelloController {
     @FXML
-    private Label welcomeText;
+    protected void onHelloButtonClick() {}
+    @FXML
+    private Text levelChoice;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private void chooseAlevel(ActionEvent event) {
+        Button clickedButton = (Button) event.getSource(); // Отримуємо кнопку, яка викликала подію
+        String buttonId = clickedButton.getId();
+
+        if ("level1".equals(buttonId)) {
+            levelChoice.setText("Level 1");
+        }else if ("level2".equals(buttonId)) {
+            levelChoice.setText("Level 2");
+        }else if ("level3".equals(buttonId)) {
+            levelChoice.setText("Level 3");
+        }else if ("level4".equals(buttonId)) {
+            levelChoice.setText("Level 4");
+        }
+        else if ("level5".equals(buttonId)) {
+            levelChoice.setText("Level5");
+        }
+        // Аналогічно для інших рівнів
+        System.out.println("Вибрано рівень: " + buttonId);
     }
 }
